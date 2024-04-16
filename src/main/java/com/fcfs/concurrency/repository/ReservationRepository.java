a621a5116a9ca50259cdac997890d710bfe4ee2e
Subin <rla0010101@gmail.com>
2024-04-16 21:42:51 +0900

    feat: repository, service ticketing 구현

---
+++ src/main/java/com/fcfs/concurrency/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.fcfs.concurrency.repository;
+
+import com.fcfs.concurrency.domain.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+}
---
+++ src/main/java/com/fcfs/concurrency/repository/TicketRepository.java
@@ -0,0 +1,15 @@
+package com.fcfs.concurrency.repository;
+
+import com.fcfs.concurrency.domain.ReservationStatus;
+import com.fcfs.concurrency.domain.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    Ticket save(Ticket ticket);
+    Optional<Ticket> findById(Long id);
+}

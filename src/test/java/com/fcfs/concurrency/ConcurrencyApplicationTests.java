a3d39c7d0c53e17bf482fb0b5202f602251ad4aa
Subin <rla0010101@gmail.com>
2024-04-16 14:45:50 +0900

    init

---
+++ src/test/java/com/fcfs/concurrency/ConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcfs.concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

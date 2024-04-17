dc6546a973b336dde29047e387772f90b725c4cd
Subin <rla0010101@gmail.com>
2024-04-17 16:32:57 +0900

    feat: 테스트 코드를 위해 상태로 티켓 찾기 위한 메소드 추가

--- src/main/java/com/fcfs/concurrency/repository/TicketRepository.java
+++ src/main/java/com/fcfs/concurrency/repository/TicketRepository.java
@@ -12,4 +12,5 @@ import java.util.Optional;
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     Ticket save(Ticket ticket);
     Optional<Ticket> findById(Long id);
+    List<Ticket> findByReservationStatus(ReservationStatus status);
 }

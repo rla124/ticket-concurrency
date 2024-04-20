1388b1f5c5ca2cf3a37f3390d3ae39f174114847
Subin <rla0010101@gmail.com>
2024-04-20 23:29:38 +0900

    feat: 순차 티켓 발행을 위한 엔티티 추가

---
+++ src/main/java/com/fcfs/concurrency/domain/TicketReservationAmount.java
@@ -0,0 +1,30 @@
+package com.fcfs.concurrency.domain;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Component
+@Getter
+@NoArgsConstructor
+public class TicketReservationAmount {
+    private int totalAmount;
+    private int reservedAmount;
+
+    public TicketReservationAmount(int totalAmount) {
+        this.totalAmount = totalAmount;
+        this.reservedAmount = 0;
+    }
+
+    public void increaseReservedAmount() {
+        this.reservedAmount++;
+    }
+
+    public boolean isPossibleToReserve(Ticket ticket) {
+        return ticket.getId() <= totalAmount;
+    }
+
+    public void setTotalAmount(int ticketAmount) {
+        totalAmount = ticketAmount;
+    }
+}
